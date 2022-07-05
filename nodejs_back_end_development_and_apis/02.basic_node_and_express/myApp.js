a20ad09a22a92cef6be10d2f1811969e8bc0d531
makz <mr.greenmousth@gmail.com>
2022-07-04 23:47:15 -0400

    nodejs day 02

--- nodejs_back_end_development_and_apis/02.basic_node_and_express/myApp.js
+++ nodejs_back_end_development_and_apis/02.basic_node_and_express/myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 let express = require('express');
 let app = express();
 
@@ -7,8 +8,13 @@ console.log('Hello World');
 //     res.send('Hello Express');
 // });
 
-app.get("/", function(req, res) {
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html')
 });
+
+const message = process.env.MESSAGE_STYLE === 'uppercase' ? {'message': 'HELLO JSON'} : {'message': 'Hello json'};
+app.get('/json', (req, res) => res.json(message));
 
 module.exports = app;
