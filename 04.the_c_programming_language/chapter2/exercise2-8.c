583114ab6314e76e6565b2b41e4bf5fe164c9113
makz mann <mr.greenmousth@gmail.com>
2023-01-16 23:27:46 -0300

    the c programming language exercise2-8 fixed

--- 04.the_c_programming_language/chapter2/exercise2-8.c
+++ 04.the_c_programming_language/chapter2/exercise2-8.c
@@ -8,8 +8,8 @@
 uint8_t rightrot(uint8_t x, uint8_t n);
 
 int main(void) {
-    uint8_t b = 45;
-    uint8_t ans = rightrot(b, 4);
+    uint8_t b = 88;
+    uint8_t ans = rightrot(b, 3); /* 11 */
 
     printf("%d\n", ans);
 
@@ -17,15 +17,15 @@ int main(void) {
 }
 
 uint8_t rightrot(uint8_t x, uint8_t n) {
-    /*                   45         4  */
-    /*             00101101            */
+    /*                   88         3  */
+    /*             01011000            */
 
-    uint8_t x1 = x & ~(~0 << n); /* 00001101 */
-    x1 <<= n;                    /* 11010000 */
+    uint8_t x1 = x & ~(~0 << n); /* 00000000 */
+    x1 <<= (sizeof(x) * 8 - n);  /* 00000000 */
 
-    uint8_t x2 = x >> n;         /* 00000010 */
+    uint8_t x2 = x >> n;         /* 00001011 */
 
-    x = x1 | x2;                 /* 11010010 */ 
+    x = x1 | x2;                 /* 00001011 */ 
 
     return x;
 }
