8168eefd417161f9f27382dff3efd5ef17385dd5
makz mann <mr.greenmousth@gmail.com>
2023-04-28 18:34:31 -0400

    full stack open - part 1: exercise 1.11 unicafe, step 6

--- 07.full-stack-open-2023/part1/unicafe/src/App.js
+++ 07.full-stack-open-2023/part1/unicafe/src/App.js
@@ -38,19 +38,26 @@ const Statistics = ({ good, neutral, bad }) => {
     const positive = good * 100 / all
 
     return (
-        <>
-            <StatisticLine text="good" value={good} />
-            <StatisticLine text="neutral" value={neutral} />
-            <StatisticLine text="bad" value={bad} />
-            <StatisticLine text="all" value={all} />
-            <StatisticLine text="average" value={average} />
-            <StatisticLine text="positive" value={positive} />
-        </>
+        <table>
+            <tbody>
+                <StatisticLine text="good" value={good} />
+                <StatisticLine text="neutral" value={neutral} />
+                <StatisticLine text="bad" value={bad} />
+                <StatisticLine text="all" value={all} />
+                <StatisticLine text="average" value={average} />
+                <StatisticLine text="positive" value={positive + ' %'} />
+            </tbody>
+        </table>
     )
 }
 
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
 
-const StatisticLine = ({ text, value }) => <p>{text} {value}</p>
+const StatisticLine = ({ text, value }) => (
+    <tr>
+        <th>{text}</th>
+        <td>{value}</td>
+    </tr>
+)
 
 export default App;
