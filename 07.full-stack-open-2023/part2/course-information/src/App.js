5422478fc26db4f664f93dab3f800c5a55717250
makz mann <mr.greenmousth@gmail.com>
2023-04-30 22:59:47 -0400

    full stack open - part 2: exercise 2.5 course information, step 10

--- 07.full-stack-open-2023/part2/course-information/src/App.js
+++ 07.full-stack-open-2023/part2/course-information/src/App.js
@@ -1,35 +1,4 @@
-// takes care of rendering the name of the course
-const Header = ({ course }) => <h2>{course}</h2>
-
-// renders the name and number of exercises of one part
-const Part = ({ name, number }) => <p>{name} {number}</p>
-
-// renders the parts and their number of exercises
-const Content = ({ parts }) => (
-    <>
-        {parts.map(part => <Part 
-            key={part.id}
-            name={part.name} 
-            number={part.exercises} />
-        )}
-    </>
-)
-
-// renders the total number of exercises
-const Total = ({ parts }) => {
-    const total = parts.reduce((total, part) => total + part.exercises, 0)
-    return <p><b>total of {total} exercises</b></p>
-}
-
-const Course = ({ course }) => {
-    return (
-        <div>
-            <Header course={course.name} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </div>
-    )
-}
+import Course from './components/Course'
 
 const App = () => {
     const courses = [
