3fa4db2d8beaf6de6b9c793a1037dd70764d8677
makz mann <mr.greenmousth@gmail.com>
2023-05-08 20:04:48 -0400

    full stack open - part 2: exercise 2.6 the phonebook, step 1

--- 07.full-stack-open-2023/part2/phonebook/src/App.js
+++ 07.full-stack-open-2023/part2/phonebook/src/App.js
@@ -1,5 +1,38 @@
+import { useState } from 'react'
+
 const App = () => {
-    return <h1>Hello, React</h1>
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas' }
+    ])
+    const [newName, setNewName] = useState('')
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        setPersons([
+            ...persons,
+            { name: newName }
+        ])
+
+        setNewName('')
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={handleSubmit} >
+                <div>
+                    name: <input value={newName} onChange={(e) => setNewName(e.target.value)}/>
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <ul>
+                { persons.map(person => <li key={person.name}>{person.name}</li>) }
+            </ul>
+        </div>
+    )
 }
 
-export default App;
+export default App
