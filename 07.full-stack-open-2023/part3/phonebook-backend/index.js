8c7aef07fb29488e7db7eef5995b8efcbed9b270
Makz Mann <mr.greenmousth@gmail.com>
2023-06-08 20:31:41 -0400

    full stack open - part 3: exercise 3.2 phonebook backend, step 2

--- 07.full-stack-open-2023/part3/phonebook-backend/index.js
+++ 07.full-stack-open-2023/part3/phonebook-backend/index.js
@@ -28,5 +28,12 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+    const date = new Date()
+    const response = `Phonebook has info for ${persons.length} people<br/>${date}`
+
+    res.send(response)
+})
+
 const PORT = 3001
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`))
