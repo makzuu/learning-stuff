5179afd2db90418726d8f9edfddde1d0844eeede
Makz Mann <mr.greenmousth@gmail.com>
2023-06-09 15:58:23 -0400

    full stack open - part 3: exercise 3.4 phonebook backend, step 4

--- 07.full-stack-open-2023/part3/phonebook-backend/index.js
+++ 07.full-stack-open-2023/part3/phonebook-backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const persons = [
+let persons = [
     {
         "id": 1,
         "name": "Arto Hellas",
@@ -43,6 +43,13 @@ app.get('/api/persons/:id', (req, res) => {
         return res.json(person)
 
     res.status(404).end()
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
 })
 
 const PORT = 3001
