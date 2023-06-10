202b4c72cdefd4de0882ce17db81394cbc5a0664
Makz Mann <mr.greenmousth@gmail.com>
2023-06-09 21:13:12 -0400

    full stack open - part 3: exercise 3.8 phonebook backend, step 8

--- 07.full-stack-open-2023/part3/phonebook-backend/index.js
+++ 07.full-stack-open-2023/part3/phonebook-backend/index.js
@@ -26,7 +26,9 @@ let persons = [
 ]
 
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (req, res) => {
     res.json(persons)
