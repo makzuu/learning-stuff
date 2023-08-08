91b0ab45cd06ada8fbb0179313875250b1105c54
Makz Mann <mr.greenmousth@gmail.com>
2023-08-07 22:10:53 -0400

    full stack open - part 3: exercise 3.22 lint configuration

--- 07.full-stack-open-2023/part3/phonebook-backend/index.js
+++ 07.full-stack-open-2023/part3/phonebook-backend/index.js
@@ -14,72 +14,72 @@ morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (req, res, next) => {
-    Person.find({})
-        .then(persons => res.json(persons))
-        .catch(error => next(error))
+  Person.find({})
+    .then(persons => res.json(persons))
+    .catch(error => next(error))
 })
 
 app.get('/info', (req, res, next) => {
-    const date = new Date()
+  const date = new Date()
 
-    Person.find({})
+  Person.find({})
     .then(persons => res.send(`Phonebook has info for ${persons.length} people</br>${date}`))
     .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id
-    Person.findById(id)
-        .then(person => {
-            if (person)
-                return res.json(person)
-            res.status(404).end()
-        }) 
-        .catch(error => next(error))
+  const id = req.params.id
+  Person.findById(id)
+    .then(person => {
+      if (person)
+        return res.json(person)
+      res.status(404).end()
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id
-    Person.findByIdAndRemove(id)
-        .then(result => res.status(204).end())
-        .catch(error => next(error))
+  const id = req.params.id
+  Person.findByIdAndRemove(id)
+    .then(result => res.status(204).end())
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res, next) => {
-    const { name, number } = req.body
+  const { name, number } = req.body
 
-    if (!name || !number)
-        return res.status(400).json({ error: 'name or number missing' })
+  if (!name || !number)
+    return res.status(400).json({ error: 'name or number missing' })
 
-    const newPerson = new Person({ name, number })
+  const newPerson = new Person({ name, number })
 
-    newPerson.save()
+  newPerson.save()
     .then(savedPerson => res.status(201).json(savedPerson))
     .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id
-    const { name, number } = req.body
+  const id = req.params.id
+  const { name, number } = req.body
 
-    if (!name || !number)
-        return res.status(400).json({ error: 'name or number missing' })
+  if (!name || !number)
+    return res.status(400).json({ error: 'name or number missing' })
 
-    Person.findByIdAndUpdate(id, { name, number }, { new: true, runValidators: true })
+  Person.findByIdAndUpdate(id, { name, number }, { new: true, runValidators: true })
     .then(updatedPerson => res.json(updatedPerson))
     .catch(error => next(error))
 })
 
 const errorHandler = (error, req, res, next) => {
-    console.error(error)
+  console.error(error)
 
-    if (error.name === 'CastError')
-        return res.status(400).json({ error: 'malformatted id' })
+  if (error.name === 'CastError')
+    return res.status(400).json({ error: 'malformatted id' })
 
-    if (error.name === 'ValidationError')
-        return res.status(400).json({ error: error.message })
+  if (error.name === 'ValidationError')
+    return res.status(400).json({ error: error.message })
 
-    next(error)
+  next(error)
 }
 
 app.use(errorHandler)
--- 07.full-stack-open-2023/part3/phonebook-backend/models/person.js
+++ 07.full-stack-open-2023/part3/phonebook-backend/models/person.js
@@ -3,30 +3,30 @@ const mongoose = require('mongoose')
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
-    .then(result => console.log('connected to MongoBD'))
-    .catch(error => console.log('error connecting to MongoDB: ', error.message))
+  .then(result => console.log('connected to MongoBD'))
+  .catch(error => console.log('error connecting to MongoDB: ', error.message))
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
+  name: {
+    type: String,
+    minLength: 3,
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: v => /^\d{2,3}-\d+$/.test(v),
+      message: props => `${props.value} is not a valid phone number!`,
     },
-    number: {
-        type: String,
-        minLength: 8,
-        validate: {
-            validator: v => /^\d{2,3}-\d+$/.test(v),
-            message: props => `${props.value} is not a valid phone number!`,
-        },
-    }
+  }
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = document._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = document._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
