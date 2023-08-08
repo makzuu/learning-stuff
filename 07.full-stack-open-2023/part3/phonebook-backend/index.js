da9e32c5251b9b80c0afdbff785718809399133b
Makz Mann <mr.greenmousth@gmail.com>
2023-08-07 20:52:40 -0400

    full stack open - part 3: exercise 3.21 deploying the database backend to production

--- 07.full-stack-open-2023/part3/phonebook-backend/index.js
+++ 07.full-stack-open-2023/part3/phonebook-backend/index.js
@@ -6,29 +6,6 @@ const cors = require('cors')
 
 const Person = require('./models/person')
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
