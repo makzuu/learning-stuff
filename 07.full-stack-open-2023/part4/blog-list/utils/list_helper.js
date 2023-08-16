69ff87f662513e360364ba94930bce30ce9d39dc
Makz Mann <mr.greenmousth@gmail.com>
2023-08-15 21:26:21 -0400

    full stack open - part 4: exercise 4.5 helper functions and unit tests, step 3

--- 07.full-stack-open-2023/part4/blog-list/utils/list_helper.js
+++ 07.full-stack-open-2023/part4/blog-list/utils/list_helper.js
@@ -2,4 +2,15 @@ const dummy = blogs => 1
 
 const totalLikes = blogs => blogs.reduce((acc, el) => acc + el.likes, 0)
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = blogs =>
+    blogs
+        .map(({ title, author, likes }) => ({ title, author, likes }))
+        .reduce((favorite, blog) => {
+            if (favorite === null)
+                return blog
+            if (blog.likes > favorite.likes)
+                return blog
+            return favorite
+        }, null)
+
+module.exports = { dummy, totalLikes, favoriteBlog }
