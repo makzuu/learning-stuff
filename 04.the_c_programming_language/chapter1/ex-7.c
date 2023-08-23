358553a4f3fbe5ad19242f705739efc19a5c55e4
Makz Mann <mr.greenmousth@gmail.com>
2023-08-23 16:44:18 -0400

    the c programming language: exercise 7, chapter I

---
+++ 04.the_c_programming_language/chapter1/ex-7.c
@@ -0,0 +1,8 @@
+/* write a program to print the value of EOF */
+
+#include <stdio.h>
+
+main()
+{
+    printf("EOF: %d\n", EOF);
+}
--- 04.the_c_programming_language/chapter1/exercise1-7.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-/* Write a program to print the value of EOF */
-main()
-{
-    printf("%d\n", EOF);
-}
