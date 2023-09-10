bf69078a8e5a5e0561554c046f12b010c82b7fbe
Makz Mann <mr.greenmousth@gmail.com>
2023-09-10 17:25:15 -0300

    full stack open - part 5: exercise 5.7 blog list frontend, step 7

--- 07.full-stack-open-2023/part5/bloglist-frontend/src/components/Blog.jsx
+++ 07.full-stack-open-2023/part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,43 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisible = () => setVisible(!visible)
+
+  const blogStyle = {
+    border: '1px solid black',
+    padding: '5px',
+    marginTop: '5px',
+  }
+
+  if (visible) {
+    return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title} {blog.author}
+          <button onClick={toggleVisible}>hide</button>
+        </div>
+        <div>
+          {blog.url}
+        </div>
+        <div>
+          {blog.likes}
+          <button>like</button>
+        </div>
+        <div>
+          {blog.user.name}
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleVisible}>view</button>
+    </div>
+  )
+}
 
 export default Blog
