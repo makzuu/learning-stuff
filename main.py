9abe0931dc9edb08ce5f819d7a0f8bf4c735d685
makzuu <mr.greenmousth@gmail.com>
2025-06-03 16:48:38 -0400

    (bootdev > asteroids): add game loop

--- main.py
+++ main.py
@@ -6,5 +6,21 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    numpass, numfail = pygame.init()
+    print(f"{numpass} initialize modules | {numfail} failed")
+
+    window_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        # input
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("noh vimo!")
+                return
+        # draw
+        window_surface.fill("black")
+        pygame.display.flip()
+
+
 if __name__ == "__main__":
     main()
