e73a624b8aaa853d061ffb68de669b20775327bc
makzuu <mr.greenmousth@gmail.com>
2025-06-03 18:05:51 -0400

    (bootdev > asteroids): cap frame rate to 60 fps

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
 
     window_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         # input
         for event in pygame.event.get():
@@ -20,6 +23,8 @@ def main():
         # draw
         window_surface.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
