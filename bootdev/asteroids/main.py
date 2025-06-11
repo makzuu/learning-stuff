50c0dc24b2af95df065fc4d7cb1dc8c2005669c4
makzuu <mr.greenmousth@gmail.com>
2025-06-11 16:44:37 -0400

    (bootdev > asteroids): group objects

--- bootdev/asteroids/main.py
+++ bootdev/asteroids/main.py
@@ -9,6 +9,12 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -16,12 +22,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
 
