461d7b0263f2e9558cd926ec775a387483f296fb
makzuu <mr.greenmousth@gmail.com>
2025-06-12 14:56:53 -0400

    (bootdev > asteroids): split asteroid into a smaller one

--- bootdev/asteroids/asteroid.py
+++ bootdev/asteroids/asteroid.py
@@ -1,10 +1,25 @@
 from circleshape import CircleShape
 import pygame
+from constants import ASTEROID_MIN_RADIUS
+import random
 
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        angle = random.uniform(20, 50)
+        v1 = self.velocity.rotate(angle)
+        v2 = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        a1 = Asteroid(self.position.x, self.position.y, new_radius)
+        a2 = Asteroid(self.position.x, self.position.y, new_radius)
+        a1.velocity = v1 * 1.2
+        a2.velocity = v2 * 1.2
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
